cc1231b0af87f14a6e24069f0b456afdc5d83a04
abrunet <abrunet@centrale-marseille.fr>
2012-11-29 17:36:50 +0100

    mille commit2

--- src/main/java/fr/todooz/util/TagCloud.java
+++ src/main/java/fr/todooz/util/TagCloud.java
@@ -1,6 +1,7 @@
 package fr.todooz.util;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import fr.todooz.http.HttpClientTest;
@@ -8,11 +9,38 @@ import fr.todooz.http.HttpClientTest;
 public class TagCloud extends HttpClientTest {
 	    private List<String> tags = new ArrayList<String>();
 
-	    public void add(String tag) {
-	        tags.add(tag);
+	    private void add(String tag){
+	    	 if (tag=="" || tag==null ){
+	    		 return;
+	    	 }
+	    	 if (!tags.contains(tag))
+	    		  tags.add(tag);	  
+	    }
+	    
+	    public void add(String ... tags) {
+	      if (tags==null) {
+	    	  return;
+	      }
+	      for (String tag : tags){
+	    	add(tag);
+	      }
 	    }
 
 	    public int size() {
-	        return tags.size();
+	        return tags.size(); 
 	    }
+
+		public boolean contains(String string) {
+			return tags.contains(string);
+		}
+
+		public void top(int i) {
+			
+			
+		}
+
+		public void shuffle() {
+			Collections.shuffle(tags);
+			
+		}
 	}
